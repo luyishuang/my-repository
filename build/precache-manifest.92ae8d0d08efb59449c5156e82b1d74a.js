1e3f28efcf0e99704704aa27a78b12783c45bb2c
luyishuang <1781990652@qq.com>
2019-11-05 20:41:23 +0800

    lu

---
+++ build/precache-manifest.92ae8d0d08efb59449c5156e82b1d74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b91845463910d0952287087b42503f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0530c5cbff5fea05c56",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "702127b2679abc1202e0",
+    "url": "/static/css/main.d64928a3.chunk.css"
+  },
+  {
+    "revision": "c0530c5cbff5fea05c56",
+    "url": "/static/js/2.c29ae654.chunk.js"
+  },
+  {
+    "revision": "702127b2679abc1202e0",
+    "url": "/static/js/main.153fd5e7.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
